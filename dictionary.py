76139b968720c035151711410c8a26be966559a2
Sam M W <you@example.com>
2025-08-10 17:00:28 +0100

    dictionary.py contains code to add entries

---
+++ dictionary.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+""" mpdule for handling the dictionary file. In particular, other scripts can
+use this to add word/links to the dictinoary. """
+
+from link import Link
+
+def append_one(entry):
+    print(entry.markdown())
+    with open("dictionary.md", "a") as f:
+        f.write(entry.markdown())
+
+def append_many(entries):
+    for entry in entries:
+        append_one(entry)
+
+def writeback(entries):
+    entries = sorted(entries)
+    with open("dictionary.md", "w") as f:
+        for entry in entries:
+            f.write(entry)
+
+def add(link):
+    entries = []
+    with open("dictionary.md", "r") as f:
+        entries = f.readlines()
+    entries += [link.markdown()]
+    writeback(entries)
+
+if __name__ == "__main__":
+    import sys
+    command = sys.argv[1]
+    if command == "check":
+        check()
+    elif command == "add":
+        lemma = sys.argv[2]
+        url = sys.argv[3]
+        add(Link(lemma, url))
+    else:
+        print(f"Unknown subcommand {command}")
+        sys.exit(1)
